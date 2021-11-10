da7ac307cb0e9a728fd2a037d5b4383d31118bb5
Ya'suf dany <yasfdany@gmail.com>
2021-11-10 14:40:54 +0700

    Login screen

--- app/src/main/java/site/studiocloud/galleryapps/ui/login/LoginActivity.kt
+++ app/src/main/java/site/studiocloud/galleryapps/ui/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package site.studiocloud.galleryapps.ui.login
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -12,15 +13,32 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        isButtonEnable(etEmail.text.toString(),etPassword.text.toString())
+
         etEmail.addTextChangedListener {
             if(it.toString().isValidEmail() && it.toString().isNotEmpty()){
                 tvEmailError.visibility = View.GONE ;
             } else {
                 tvEmailError.visibility = View.VISIBLE;
             }
+
+            isButtonEnable(it.toString(),etPassword.text.toString())
+        }
+        etPassword.addTextChangedListener{
+            isButtonEnable(etEmail.text.toString(),it.toString())
         }
     }
 
-    fun String.isValidEmail() =
+    private fun isButtonEnable(email : String, password: String){
+        btLogin.isEnabled =  email.isValidEmail() && email.isNotEmpty() && password.isNotEmpty();
+        if (btLogin.isEnabled){
+            btLogin.setBackgroundColor(resources.getColor(R.color.purple_500))
+        } else {
+            btLogin.setBackgroundColor(Color.parseColor("#afafaf"))
+        }
+    }
+
+    private fun String.isValidEmail() =
         isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
 }
