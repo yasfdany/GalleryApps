b5387053a560cc4b60029efbe6da0062c9e3159c
Ya'suf dany <yasfdany@gmail.com>
2021-11-10 21:47:13 +0700

    Validate password, login session

--- app/src/main/java/dev/studiocloud/galleryapps/ui/login/LoginActivity.kt
+++ app/src/main/java/dev/studiocloud/galleryapps/ui/login/LoginActivity.kt
@@ -12,12 +12,22 @@ import dev.studiocloud.galleryapps.R
 import dev.studiocloud.galleryapps.ui.home.HomeActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import java.util.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 import kotlin.concurrent.schedule
+import android.content.SharedPreferences
+
+
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var prefsEditor : SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        prefsEditor = getSharedPreferences("GALLERY_APPS", MODE_PRIVATE).edit()
 
         isButtonEnable(etEmail.text.toString(),etPassword.text.toString())
 
@@ -32,6 +42,12 @@ class LoginActivity : AppCompatActivity() {
         }
 
         etPassword.addTextChangedListener{
+            if (it.toString().isValidPassword() && it.toString().length >= 6 && it.toString().isNotEmpty()){
+                tvPasswordError.visibility = View.GONE
+            } else {
+                tvPasswordError.visibility = View.VISIBLE
+            }
+
             isButtonEnable(etEmail.text.toString(),it.toString())
         }
 
@@ -39,8 +55,11 @@ class LoginActivity : AppCompatActivity() {
             val progressDialog = createDialog()
             progressDialog.show()
             Timer().schedule(3000){
+                prefsEditor.putBoolean("LOGGED",true)
+                prefsEditor.apply()
                 progressDialog.dismiss()
                 startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+                finish()
             }
         }
     }
@@ -52,12 +71,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun isButtonEnable(email : String, password: String){
-        btLogin.isEnabled =  email.isValidEmail() && email.isNotEmpty() && password.isNotEmpty()
+        btLogin.isEnabled =  email.isValidEmail() && email.isNotEmpty() && password.isValidPassword() && password.length >= 6 && password.isNotEmpty()
         if (btLogin.isEnabled){
             btLogin.setBackgroundColor(resources.getColor(R.color.purple_500))
         } else {
             btLogin.setBackgroundColor(Color.parseColor("#afafaf"))
         }
+    }
+
+    private fun String.isValidPassword(): Boolean {
+        val pattern: Pattern
+        val passwordPattern = "^(?=.*[A-Z]).{4,}\$"
+        pattern = Pattern.compile(passwordPattern)
+        val matcher: Matcher = pattern.matcher(this)
+        return matcher.matches()
     }
 
     private fun String.isValidEmail() =
--- app/src/main/java/dev/studiocloud/galleryapps/ui/splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/dev/studiocloud/galleryapps/ui/splashscreen/SplashscreenActivity.kt
@@ -11,10 +11,17 @@ import dev.studiocloud.galleryapps.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_splashscreen.*
 import java.util.*
 import kotlin.concurrent.schedule
+import android.content.SharedPreferences
+import dev.studiocloud.galleryapps.ui.home.HomeActivity
+
 
 class SplashscreenActivity : AppCompatActivity() {
+    private lateinit var prefs : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        prefs = getSharedPreferences("GALLERY_APPS", MODE_PRIVATE)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -30,7 +37,12 @@ class SplashscreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splashscreen)
 
         Timer().schedule(1000){
-            startActivity(Intent(this@SplashscreenActivity, LoginActivity::class.java))
+            if (prefs.getBoolean("LOGGED",false)){
+                startActivity(Intent(this@SplashscreenActivity, HomeActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashscreenActivity, LoginActivity::class.java))
+            }
+            finish()
         }
     }
 }
